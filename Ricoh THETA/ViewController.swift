937c60a9233058ffb747158e0fe1044069b4741b
shimokp <oxo_inflove@yahoo.co.jp>
2016-08-04 00:21:33 +0900

    Initial Commit

---
+++ Ricoh THETA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ricoh THETA
+//
+//  Created by 下村一将 on 2016/08/04.
+//  Copyright © 2016年 kazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
